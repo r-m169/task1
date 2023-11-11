b57751032d0ef6c7fc18481d7ea52777fc1d9d24
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-11-11 08:38:13 +0200

    install nodemodules

---
+++ your-app-name/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1><span>R</span>ODAYN<span>A</span></h1>
+    </div>
+  );
+}
+
+export default App;
